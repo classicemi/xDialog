a69c9ba50580a7599531424a63e233352a6099d8
classicemi <wushuang_1227@163.com>
2014-07-31 18:00:46 +0800

    init

---
+++ src/xDialog.js
@@ -0,0 +1,21 @@
+(function() {
+
+	var XDialog = function(options) {
+
+		var defaultOptions = {
+			content: ''
+		};
+
+		var opt = $.extend(defaultOptions, options);
+
+	};
+
+	XDialog.prototype = {
+
+		show: function() {
+
+		}
+
+	};
+
+})();
